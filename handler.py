40eddd074e96724f5265d2eb5aaa631b6e30f4f7
hrg921 <hrg921@gmail.com>
2018-08-27 16:48:13 +0900

    Revert "Update RelatedTagAPI using ElasticSearch (#1)"
    This reverts commit c0b69d8494e31f7b28fa909701d20332e09d2fc5.

--- handler.py
+++ handler.py
@@ -5,18 +5,12 @@ import elastic_search
 def main(event, context):
     params = event['queryStringParameters']
     print(params)
-    if type(params["tag"]) is not list:
+    if params["tag"] is not list:
         params["tag"] = [params["tag"]]
     if params.get("sort") is not "co_occur":
         params["sort"] = "relatedwords.genyear"
 
-    if params.get("version") == 0:
-        result = elastic_search.get(params)
-    elif params.get("version") == 1:
-        params['size'] = params['size'] / 2.0
-        result = elastic_search.get_by_graphapi(params)
-    elif params.get("version") == 2:
-        result = elastic_search.get_by_significant_term(params)
+    result = elastic_search.get(params)
 
     response = {
         "statusCode": 200,
@@ -24,3 +18,16 @@ def main(event, context):
     }
 
     return response
+
+
+# def get_related_keywords(input_,co_occur=True, size=10, order='desc'):
+#     """
+#       input_ : 'machine learning', 'internet of things (iot)', 'machine learning+internet of things (iot)'
+#                 만 가능, 주의-keyword로 검색해서 spacebar 하나달라도 다른 글자로 인식함
+#     """
+#     if input_ is not list:
+#       input_ = [input_]
+#     sort = 'relatedwords.co_occur' if co_occur else 'relatedwords.genyear'
+#     params = { 'input' : input_, 'sort' : sort, 'order' : order, 'size' : size}
+#
+#     return request(params)
