c0b69d8494e31f7b28fa909701d20332e09d2fc5
lis123kr <ls123kr@naver.com>
2018-08-27 16:32:58 +0900

    Update RelatedTagAPI using ElasticSearch (#1)
    Elastic graph api & aggregation

--- elastic_search.py
+++ elastic_search.py
@@ -5,10 +5,14 @@ import requests
 import json
 import query
 
-
 def get(params):
     return request(params)
 
+def get_by_graphapi(params):
+    return request_v1(params)
+
+def get_by_significant_term(params):
+    return request_v2(params)
 
 def request(params):
     """
@@ -29,4 +33,24 @@ def request(params):
     hits = json.loads(r.text)['hits']['hits']
     if len(hits) > 0:
         return [h['_source']['relatedwords'] for h in hits]
+    return []
+
+def request_v1(params):
+    URL = 'https://fd2b16254da343159c56f09ad393c420.us-west-1.aws.found.io:9243/papers/_xpack/graph/_explore'
+    headers = {'Accept': 'text/plain', 'Content-type': 'application/json'}
+
+    r = requests.get(URL, data=json.dumps(query.get_by_graphapi(params)), headers=headers, auth=('elastic', 'Ftkn0jSUxwI867OzNmPiAVeu'))
+    hits = json.loads(r.text)['vertices']
+    if len(hits) > 0:
+        return [h['term'] for h in hits]
+    return []
+
+def request_v2(params):
+    URL = 'https://fd2b16254da343159c56f09ad393c420.us-west-1.aws.found.io:9243/papers/doc/_search'
+    headers = {'Accept': 'text/plain', 'Content-type': 'application/json'}
+
+    r = requests.get(URL, data=json.dumps(query.get_by_agg_signicant_terms(params)), headers=headers, auth=('elastic', 'Ftkn0jSUxwI867OzNmPiAVeu'))
+    hits = json.loads(r.text)['aggregations']['significant_related_word']['buckets']
+    if len(hits) > 0:
+        return [h['key'] for h in hits]
     return []
--- query.py
+++ query.py
@@ -1,7 +1,7 @@
 """
 Created by hangeonho on 2018. 8. 22..
 """
-
+import ast
 
 def get(params):
     query = {
@@ -22,5 +22,76 @@ def get(params):
                 }
             }
         ]
+    }
+    return query
+
+def get_by_graphapi(params):
+  """
+    "tag" : list of keywords
+    "date" : papers starting from params['date']
+    "min_doc_count" : The minimum frequency of keywords in the entire document.
+  """
+    query = {
+        "query": {
+            "bool": {
+              "must":
+                list(ast.literal_eval(', '.join( [ str({ "match" : { "keywords_tag" :  i  } }) for i in params.get('tag') ] )))
+            }
+        },
+        "controls": {
+            "sample_size": 75001,
+            "timeout": 5000
+        },
+        "connections": {
+              "query" : {
+                "bool": {
+                  "filter": [
+                   {
+                    "range": {
+                       "start_date.min_date": {
+                          "gte": params.get('date')
+                       }
+                    }
+                   }
+                  ]
+                 }
+              },
+                "vertices": [
+                    {
+                        "field": "keywords_tag",
+                        "size": params.get('size', 8),
+                        "min_doc_count": params.get('min_doc_count'),
+                        "exclude" : params.get('tag')
+                    }
+                ]
+        },
+        "vertices": [
+            {
+                "field": "keywords_tag",
+                "size": params.get('size', 8),
+                "min_doc_count": params.get('min_doc_count'),
+                "exclude" : params.get('tag')
+            }
+        ]
+    }
+    return query
+
+def get_by_agg_signicant_terms(params):
+    query = {
+      "size": 0, 
+        "query" : {
+          "bool": {
+            "must": 
+              list(ast.literal_eval(', '.join( [ str({ "match" : { "keywords_tag" :  i  } }) for i in params.get('tag') ] )))
+          }
+        },
+        "aggregations" : {
+            "significant_related_word" : {
+              "significant_terms" : { 
+                "field" : "keywords_tag",
+                "exclude": params.get("tag")
+              }
+            }
+        }
     }
     return query
